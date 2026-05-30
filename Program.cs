8e86fa35bdee0561cd5a8fc6b2b2a3140da6563a
Joseph <jp982774414@gmail.com>
2026-05-29 21:36:17 -0500

    Redireccionamiento automatico al swagger

--- Program.cs
+++ Program.cs
@@ -22,7 +22,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.MapGet("/", () => Results.Redirect("/swagger"));
 app.MapControllers();
 app.UseSwagger();
 app.UseSwaggerUI();
